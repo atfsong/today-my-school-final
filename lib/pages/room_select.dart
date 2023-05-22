57fe84166d4036437bf085bf48e95c1ffd6d3e43
songhyunjeong <toito1toi@g.kmou.ac.kr>
2023-05-23 07:46:08 +0900

    💄 예약 장소 선택 페이지 UI 수정

--- lib/pages/room_select.dart
+++ lib/pages/room_select.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:today_my_school_final/samples/room.dart';
+import 'package:today_my_school_final/data/room.dart';
+import 'package:today_my_school_final/pages/reservation_form.dart';
 import 'package:today_my_school_final/style.dart';
 
 class RoomSelectPage extends StatefulWidget {
@@ -41,31 +42,34 @@ class _RoomSelectPageState extends State<RoomSelectPage> {
                 ),
               ),
               Positioned(
-                child: Padding(
+                child: ListView.separated(
                   padding: EdgeInsets.symmetric(horizontal: 40.w),
-                  child: ListView.separated(
-                    separatorBuilder: (context, index) {
-                      return SizedBox(
-                        height: 16.h,
-                      );
-                    },
-                    itemCount: rooms.length,
-                    itemBuilder: (context, index) {
-                      return ReservationStatusCard(
-                        place: rooms[index].place,
-                        location: rooms[index].location,
-                        maxCapacity: rooms[index].maxCapacity,
-                        image: rooms[index].image,
-                        isAvailable: const {
-                          '1': 'true',
-                          '2': 'true',
-                          '3': 'false',
-                          '4': 'false',
-                          '5': 'true',
-                        },
-                      );
-                    },
-                  ),
+                  separatorBuilder: (context, index) {
+                    return SizedBox(
+                      height: 16.h,
+                    );
+                  },
+                  itemCount: rooms.length,
+                  itemBuilder: (context, index) {
+                    return ReservationStatusCard(
+                      place: rooms[index].place,
+                      location: rooms[index].location,
+                      maxCapacity: rooms[index].maxCapacity,
+                      image: rooms[index].image,
+                      isAvailable: const [
+                        true,
+                        true,
+                        false,
+                        false,
+                        true,
+                        true,
+                        true,
+                        false,
+                        true
+                      ],
+                      index: index,
+                    );
+                  },
                 ),
               ),
             ],
@@ -81,7 +85,8 @@ class ReservationStatusCard extends StatefulWidget {
   final String location;
   final int maxCapacity;
   final String image;
-  Map isAvailable;
+  List<bool> isAvailable;
+  final int index;
 
   ReservationStatusCard({
     super.key,
@@ -90,6 +95,7 @@ class ReservationStatusCard extends StatefulWidget {
     required this.maxCapacity,
     required this.image,
     required this.isAvailable,
+    required this.index,
   });
 
   @override
@@ -99,102 +105,96 @@ class ReservationStatusCard extends StatefulWidget {
 class _ReservationStatusCardState extends State<ReservationStatusCard> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: 120.h,
-      decoration: BoxDecoration(
-        color: ColorPalette.white,
-        borderRadius: BorderRadius.circular(10),
-        boxShadow: const <BoxShadow>[
-          BoxShadow(
-            color: Color.fromRGBO(0, 0, 0, 0.25),
-            offset: Offset(0, 4),
-            blurRadius: 4,
-            spreadRadius: 0,
-          ),
-        ],
-      ),
-      child: Column(
-        children: [
-          Row(
-            children: [
-              Padding(
-                padding: EdgeInsets.symmetric(horizontal: 16.w, vertical: 16.h),
-                child: ClipRRect(
-                  borderRadius: BorderRadius.circular(10),
-                  child: SizedBox(
-                    width: 72.w,
-                    height: 72.h,
-                    child: Image.asset(
-                      widget.image,
-                      fit: BoxFit.fitHeight,
+    return GestureDetector(
+      onTap: () {
+        Navigator.of(context).push(MaterialPageRoute(
+            builder: (context) => ReservationForm(room: rooms[widget.index])));
+        //Navigator.of(context).pushNamed('/reservation_form',arguments: ReservationForm(room: rooms[widget.index]));
+      },
+      child: Container(
+        height: 120.h,
+        decoration: BoxDecoration(
+          color: ColorPalette.white,
+          borderRadius: BorderRadius.circular(10),
+          boxShadow: const <BoxShadow>[
+            BoxShadow(
+              color: Color.fromRGBO(0, 0, 0, 0.25),
+              offset: Offset(0, 4),
+              blurRadius: 4,
+              spreadRadius: 0,
+            ),
+          ],
+        ),
+        child: Column(
+          children: [
+            Row(
+              children: [
+                Padding(
+                  padding:
+                      EdgeInsets.symmetric(horizontal: 16.w, vertical: 16.h),
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.circular(10),
+                    child: SizedBox(
+                      width: 72.w,
+                      height: 72.h,
+                      child: Image.asset(
+                        widget.image,
+                        fit: BoxFit.fitHeight,
+                      ),
                     ),
                   ),
                 ),
-              ),
-              Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    widget.place,
-                    style: TextStyleSet.medium15,
-                  ),
-                  Text(
-                    widget.location,
-                    style: TextStyleSet.light13,
-                  ),
-                  Text(
-                    '최대 ${widget.maxCapacity}명 이용 가능',
-                    style: TextStyleSet.light13,
-                  ),
-                ],
-              ),
-            ],
-          ),
-          SizedBox(
-            height: 4.h,
-            child: ListView.separated(
-              scrollDirection: Axis.horizontal,
-              separatorBuilder: (context, index) {
-                return SizedBox(width: 2.w);
-              },
-              itemCount: widget.isAvailable.length,
-              itemBuilder: (context, index) {
-                for (var value in widget.isAvailable.values) {
-                  if (value == 'true') {
-                    StatusBar(
-                      color: ColorPalette.green,
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      widget.place,
+                      style: TextStyleSet.medium15,
+                    ),
+                    Text(
+                      widget.location,
+                      style: TextStyleSet.light13,
+                    ),
+                    Text(
+                      '최대 ${widget.maxCapacity}명 이용 가능',
+                      style: TextStyleSet.light13,
+                    ),
+                  ],
+                ),
+              ],
+            ),
+            SizedBox(
+              height: 4.h,
+              child: ListView.separated(
+                padding: EdgeInsets.symmetric(horizontal: 16.w),
+                scrollDirection: Axis.horizontal,
+                separatorBuilder: (context, index) {
+                  return SizedBox(width: 2.w);
+                },
+                itemCount: widget.isAvailable.length,
+                itemBuilder: (context, index) {
+                  if (widget.isAvailable[index]) {
+                    return Container(
+                      width: 28.w,
+                      height: 4.h,
+                      decoration: const BoxDecoration(
+                        color: ColorPalette.green,
+                      ),
                     );
                   }
-                  return StatusBar(
-                    color: ColorPalette.grey,
+                  return Container(
+                    width: 28.w,
+                    height: 4.h,
+                    decoration: const BoxDecoration(
+                      color: ColorPalette.lightGrey,
+                    ),
                   );
-                }
-                return null;
-              },
+                },
+              ),
             ),
-          ),
-        ],
-      ),
-    );
-  }
-}
-
-class StatusBar extends StatelessWidget {
-  Color color;
-
-  StatusBar({
-    super.key,
-    required this.color,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      width: 24.w,
-      height: 4.h,
-      decoration: BoxDecoration(
-        color: color,
+          ],
+        ),
       ),
     );
   }
