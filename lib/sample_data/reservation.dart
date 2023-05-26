027a2758b3c289530849917f9f0c1603b2dcd128
songhyunjeong <toito1toi@g.kmou.ac.kr>
2023-05-26 16:40:34 +0900

    🚚 파일 이름 변경

--- lib/sample_data/reservation.dart
+++ lib/sample_data/reservation.dart
@@ -1,24 +1,4 @@
-class Reservation {
-  final String place;
-  final DateTime date;
-  final DateTime startTime;
-  final DateTime endTime;
-  final int numOfPeople;
-  final String purpose;
-  final String uid;
-  final String rid;
-
-  Reservation({
-    required this.place,
-    required this.date,
-    required this.startTime,
-    required this.endTime,
-    required this.numOfPeople,
-    required this.purpose,
-    required this.uid,
-    required this.rid,
-  });
-}
+import 'package:today_my_school_final/models/model_reservation.dart';
 
 List<Reservation> reservations = [
   Reservation(
